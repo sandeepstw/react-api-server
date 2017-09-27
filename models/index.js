afad14fd33db820ee1ac744bafa81d04d6f9baf3
swati <swatee.chavan@gmail.com>
2017-09-27 17:14:00 +0530

    API

--- models/index.js
+++ models/index.js
@@ -1,11 +1,8 @@
 const mongoose = require('mongoose');
-
 module.exports.connect = (uri) => {
   mongoose.connect(uri);
   // plug in the promise library:
   mongoose.Promise = global.Promise;
-
-
   mongoose.connection.on('error', (err) => {
     console.error(`Mongoose connection error: ${err}`);
     process.exit(1);
